aa6d402bf06c84be1f15ef5071672e4b5e24ffaa
eugenekravchuk <jenia.kravchuk223@gmail.com>
2025-05-18 14:07:29 +0300

    separated article creation

--- src/pages/CreateArticle.tsx
+++ src/pages/CreateArticle.tsx
@@ -7,6 +7,8 @@ import { db } from "../data/firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { uploadToCloudinary } from "../utils/uploadToCloudinary";
 import { ChooseAuthor } from "../components/articleCreation/ChooseAuthor";
+import { CreateAuthorForm } from "../components/articleCreation/CreateAuthorForm";
+import { ArticleForm } from "../components/articleCreation/ArticleFrom";
 
 const labels = ["ART", "SCULPTURE", "STREET ART"];
 const readTimes = ["5 min", "10 min", "15 min"];
@@ -231,234 +233,250 @@ const CreateArticle = () => {
           // </div>
         )}
         {step === "new" && (
-          <div className="flex flex-col md:flex-row gap-10 items-start w-full max-w-4xl">
-            <label
-              htmlFor="photo-upload"
-              className="w-64 h-80 border-2 border-black border-dashed flex items-center justify-center cursor-pointer text-center"
-            >
-              {photoAuthor ? (
-                <img
-                  src={URL.createObjectURL(photoAuthor)}
-                  alt="Selected"
-                  className="w-full h-full object-cover"
-                />
-              ) : (
-                <div>
-                  <p className="text-xl font-medium">+</p>
-                  <p className="text-lg">Add photo</p>
-                </div>
-              )}
-              <input
-                type="file"
-                id="photo-upload"
-                accept="image/*"
-                className="hidden"
-                onChange={handlePhotoAuthorChange}
-              />
-            </label>
-
-            <div className="flex flex-col gap-4 w-full flex-1">
-              <input
-                placeholder="Name Surname"
-                className="border px-4 py-2 rounded"
-                onChange={(e) =>
-                  setNewAuthor((prev) => ({ ...prev, name: e.target.value }))
-                }
-              />
-              <input
-                placeholder="Job (e.g. artist)"
-                className="border px-4 py-2 rounded"
-                onChange={(e) =>
-                  setNewAuthor((prev) => ({ ...prev, job: e.target.value }))
-                }
-              />
-              <textarea
-                placeholder="Best skills (3-4 sentences)"
-                className="border px-4 py-2 rounded resize-none"
-                rows={3}
-                onChange={(e) =>
-                  setNewAuthor((prev) => ({
-                    ...prev,
-                    bold_info: e.target.value,
-                  }))
-                }
-              />
-              <textarea
-                placeholder="About (4+ sentences)"
-                className="border px-4 py-2 rounded resize-none"
-                rows={4}
-                onChange={(e) =>
-                  setNewAuthor((prev) => ({ ...prev, info: e.target.value }))
-                }
-              />
-
-              <div className="flex gap-4 mt-4">
-                <button
-                  className="px-6 py-2 border border-black rounded"
-                  onClick={goBack}
-                >
-                  Back
-                </button>
-                <button
-                  className="bg-black text-white px-6 py-2 rounded font-semibold"
-                  onClick={handleCreateAuthor}
-                >
-                  Save and Continue
-                </button>
-              </div>
-            </div>
-          </div>
+          <CreateAuthorForm
+            photoAuthor={photoAuthor}
+            handlePhotoAuthorChange={handlePhotoAuthorChange}
+            newAuthor={newAuthor}
+            setNewAuthor={setNewAuthor}
+            handleCreateAuthor={handleCreateAuthor}
+            goBack={goBack}
+          />
+          // <div className="flex flex-col md:flex-row gap-10 items-start w-full max-w-4xl">
+          //   <label
+          //     htmlFor="photo-upload"
+          //     className="w-64 h-80 border-2 border-black border-dashed flex items-center justify-center cursor-pointer text-center"
+          //   >
+          //     {photoAuthor ? (
+          //       <img
+          //         src={URL.createObjectURL(photoAuthor)}
+          //         alt="Selected"
+          //         className="w-full h-full object-cover"
+          //       />
+          //     ) : (
+          //       <div>
+          //         <p className="text-xl font-medium">+</p>
+          //         <p className="text-lg">Add photo</p>
+          //       </div>
+          //     )}
+          //     <input
+          //       type="file"
+          //       id="photo-upload"
+          //       accept="image/*"
+          //       className="hidden"
+          //       onChange={handlePhotoAuthorChange}
+          //     />
+          //   </label>
+          //   <div className="flex flex-col gap-4 w-full flex-1">
+          //     <input
+          //       placeholder="Name Surname"
+          //       className="border px-4 py-2 rounded"
+          //       onChange={(e) =>
+          //         setNewAuthor((prev) => ({ ...prev, name: e.target.value }))
+          //       }
+          //     />
+          //     <input
+          //       placeholder="Job (e.g. artist)"
+          //       className="border px-4 py-2 rounded"
+          //       onChange={(e) =>
+          //         setNewAuthor((prev) => ({ ...prev, job: e.target.value }))
+          //       }
+          //     />
+          //     <textarea
+          //       placeholder="Best skills (3-4 sentences)"
+          //       className="border px-4 py-2 rounded resize-none"
+          //       rows={3}
+          //       onChange={(e) =>
+          //         setNewAuthor((prev) => ({
+          //           ...prev,
+          //           bold_info: e.target.value,
+          //         }))
+          //       }
+          //     />
+          //     <textarea
+          //       placeholder="About (4+ sentences)"
+          //       className="border px-4 py-2 rounded resize-none"
+          //       rows={4}
+          //       onChange={(e) =>
+          //         setNewAuthor((prev) => ({ ...prev, info: e.target.value }))
+          //       }
+          //     />
+          //     <div className="flex gap-4 mt-4">
+          //       <button
+          //         className="px-6 py-2 border border-black rounded"
+          //         onClick={goBack}
+          //       >
+          //         Back
+          //       </button>
+          //       <button
+          //         className="bg-black text-white px-6 py-2 rounded font-semibold"
+          //         onClick={handleCreateAuthor}
+          //       >
+          //         Save and Continue
+          //       </button>
+          //     </div>
+          //   </div>
+          // </div>
         )}
 
         {step === "article" && (
-          <div className="space-y-6 max-w-4xl">
-            <label
-              htmlFor="article-photo"
-              className="w-full h-60 border-2 border-black border-dashed flex items-center justify-center cursor-pointer text-center"
-            >
-              {photoArticle ? (
-                <img
-                  src={URL.createObjectURL(photoArticle)}
-                  alt="Selected"
-                  className="w-full h-full object-cover"
-                />
-              ) : (
-                <div>
-                  <p className="text-2xl font-medium">+</p>
-                  <p className="text-lg">Add article photo</p>
-                </div>
-              )}
-              <input
-                type="file"
-                id="article-photo"
-                accept="image/*"
-                className="hidden"
-                onChange={handlePhotoArticleChange}
-              />
-            </label>
+          <ArticleForm
+            articleData={articleData}
+            setArticleData={setArticleData}
+            photoArticle={photoArticle}
+            handlePhotoArticleChange={handlePhotoArticleChange}
+            labels={labels}
+            readTimes={readTimes}
+            handleFinish={handleFinish}
+            goBack={goBack}
+          />
+          // <div className="space-y-6 max-w-4xl">
+          //   <label
+          //     htmlFor="article-photo"
+          //     className="w-full h-60 border-2 border-black border-dashed flex items-center justify-center cursor-pointer text-center"
+          //   >
+          //     {photoArticle ? (
+          //       <img
+          //         src={URL.createObjectURL(photoArticle)}
+          //         alt="Selected"
+          //         className="w-full h-full object-cover"
+          //       />
+          //     ) : (
+          //       <div>
+          //         <p className="text-2xl font-medium">+</p>
+          //         <p className="text-lg">Add article photo</p>
+          //       </div>
+          //     )}
+          //     <input
+          //       type="file"
+          //       id="article-photo"
+          //       accept="image/*"
+          //       className="hidden"
+          //       onChange={handlePhotoArticleChange}
+          //     />
+          //   </label>
 
-            <input
-              type="text"
-              placeholder="Title"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.title}
-              onChange={(e) =>
-                setArticleData({ ...articleData, title: e.target.value })
-              }
-            />
+          //   <input
+          //     type="text"
+          //     placeholder="Title"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.title}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, title: e.target.value })
+          //     }
+          //   />
 
-            <input
-              type="text"
-              placeholder="Description"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.description}
-              onChange={(e) =>
-                setArticleData({ ...articleData, description: e.target.value })
-              }
-            />
+          //   <input
+          //     type="text"
+          //     placeholder="Description"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.description}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, description: e.target.value })
+          //     }
+          //   />
 
-            <select
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.readTime}
-              onChange={(e) =>
-                setArticleData({ ...articleData, readTime: e.target.value })
-              }
-            >
-              {readTimes.map((time) => (
-                <option key={time} value={time}>
-                  {time}
-                </option>
-              ))}
-            </select>
+          //   <select
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.readTime}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, readTime: e.target.value })
+          //     }
+          //   >
+          //     {readTimes.map((time) => (
+          //       <option key={time} value={time}>
+          //         {time}
+          //       </option>
+          //     ))}
+          //   </select>
 
-            <select
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.label}
-              onChange={(e) =>
-                setArticleData({ ...articleData, label: e.target.value })
-              }
-            >
-              <option value="" disabled>
-                Label
-              </option>
-              {labels.map((label) => (
-                <option key={label} value={label}>
-                  {label}
-                </option>
-              ))}
-            </select>
+          //   <select
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.label}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, label: e.target.value })
+          //     }
+          //   >
+          //     <option value="" disabled>
+          //       Label
+          //     </option>
+          //     {labels.map((label) => (
+          //       <option key={label} value={label}>
+          //         {label}
+          //       </option>
+          //     ))}
+          //   </select>
 
-            <textarea
-              placeholder="Bold Info 1"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.bold_info1}
-              onChange={(e) =>
-                setArticleData({ ...articleData, bold_info1: e.target.value })
-              }
-            />
+          //   <textarea
+          //     placeholder="Bold Info 1"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.bold_info1}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, bold_info1: e.target.value })
+          //     }
+          //   />
 
-            <textarea
-              placeholder="Info 1"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.info1}
-              onChange={(e) =>
-                setArticleData({ ...articleData, info1: e.target.value })
-              }
-            />
+          //   <textarea
+          //     placeholder="Info 1"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.info1}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, info1: e.target.value })
+          //     }
+          //   />
 
-            <textarea
-              placeholder="Bold Info 2"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.bold_info2}
-              onChange={(e) =>
-                setArticleData({ ...articleData, bold_info2: e.target.value })
-              }
-            />
+          //   <textarea
+          //     placeholder="Bold Info 2"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.bold_info2}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, bold_info2: e.target.value })
+          //     }
+          //   />
 
-            <textarea
-              placeholder="Info 2"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.info2}
-              onChange={(e) =>
-                setArticleData({ ...articleData, info2: e.target.value })
-              }
-            />
+          //   <textarea
+          //     placeholder="Info 2"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.info2}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, info2: e.target.value })
+          //     }
+          //   />
 
-            <input
-              type="text"
-              placeholder="Quote"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.quote}
-              onChange={(e) =>
-                setArticleData({ ...articleData, quote: e.target.value })
-              }
-            />
+          //   <input
+          //     type="text"
+          //     placeholder="Quote"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.quote}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, quote: e.target.value })
+          //     }
+          //   />
 
-            <input
-              type="text"
-              placeholder="Quote Author"
-              className="w-full border px-4 py-2 rounded"
-              value={articleData.quote_author}
-              onChange={(e) =>
-                setArticleData({ ...articleData, quote_author: e.target.value })
-              }
-            />
+          //   <input
+          //     type="text"
+          //     placeholder="Quote Author"
+          //     className="w-full border px-4 py-2 rounded"
+          //     value={articleData.quote_author}
+          //     onChange={(e) =>
+          //       setArticleData({ ...articleData, quote_author: e.target.value })
+          //     }
+          //   />
 
-            <div className="flex gap-4 justify-center">
-              <button
-                className="px-6 py-2 border border-black rounded"
-                onClick={goBack}
-              >
-                Back
-              </button>
-              <button
-                className="bg-black text-white px-6 py-2 rounded font-semibold"
-                onClick={handleFinish}
-              >
-                Finish
-              </button>
-            </div>
-          </div>
+          //   <div className="flex gap-4 justify-center">
+          //     <button
+          //       className="px-6 py-2 border border-black rounded"
+          //       onClick={goBack}
+          //     >
+          //       Back
+          //     </button>
+          //     <button
+          //       className="bg-black text-white px-6 py-2 rounded font-semibold"
+          //       onClick={handleFinish}
+          //     >
+          //       Finish
+          //     </button>
+          //   </div>
+          // </div>
         )}
       </div>
 
